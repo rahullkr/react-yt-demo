4f1d75b93c89f453fa52405d50dc6684d7ae9beb
rahullkr <rk7318323176@gmail.com>
2023-09-26 02:56:46 +0530

    added usereduce in some parts

--- src/App.js
+++ src/App.js
@@ -1,28 +1,56 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
 import "./App.css";
 import data from "./data/data";
 import AddVideo from "./components/Addvideo";
 import VideoList from "./components/Videolist";
 function App() {
-  const [videos, setVideos] = useState(data);
+
   const [editableVideo, seteditableVideo] = useState(null);
+
+  function videoReducer(videos, action){
+    switch(action.type){
+      case 'ADD':
+        return [...videos, { ...action.payload, id: videos.length + 1 }]
+      case 'DELETE': 
+        return videos.filter((video) => video.id !== action.payload);
+      case 'UPDATE': 
+         const index = videos.findIndex((v) => v.id === action.payload.id);
+         const newVideo = [...videos];
+         newVideo.splice(index, 1, action.payload );
+         seteditableVideo(null);
+         return newVideo;
+      
+        default : 
+        return videos; 
+    }
+
+  }
+ // to make a usereducer state we describe it like this
+ const [videos, dispatch] = useReducer(videoReducer, data)
+ // here reducer is a fn and data is the data passed and dispatch is a fn which will provide flexibility 
+
+  // const [videos, setVideos] = useState(data);
+
   function addVideos(video) {
-    setVideos([...videos, { ...video, id: videos.length + 1 }]);
+      dispatch({type: 'ADD', payload: video})
+    // setVideos([...videos, { ...video, id: videos.length + 1 }]);
   }
 
   function deleteVideo(id) {
     // console.log(id);
-    setVideos(videos.filter((video) => video.id !== id));
+    dispatch({type: 'DELETE', payload: id})
+    // setVideos(videos.filter((video) => video.id !== id));
   }
 
   function editVideo(id) {
     seteditableVideo(videos.find((video) => video.id === id));
   }
   function updateVideo(video) {
-    const index = videos.findIndex((v) => v.id === video.id);
-    const newVideo = [...videos];
-    newVideo.splice(index, 1, video);
-    setVideos(newVideo);
+    dispatch({type: 'UPDATE', payload: video})
+    // const index = videos.findIndex((v) => v.id === video.id);
+    // const newVideo = [...videos];
+    // newVideo.splice(index, 1, video);
+    // setVideos(newVideo);
     // console.log(newVideo);
   }
   return (
